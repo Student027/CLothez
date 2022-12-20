61174c8974178b9862024e8f4d7c2cb8ac5d4209
Student027 <95233877+Student027@users.noreply.github.com>
2022-12-20 22:45:59 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,332 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+/*
+(づ｡◕‿‿◕｡)づ
+༼ つ ◕‿◕ ༽つ
+(づ￣ ³￣)づ
+(つ≧▽≦)つ
+(つ✧ω✧)つ
+*/
+'use strict';
+
+function itemForm() {
+  document.getElementById("StatusForm").style.display = "block";
+}
+
+function deleteform(){
+}
+
+function itemdesc(){
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeitemdesc(){
+  document.getElementById("myForm").style.display = "none";
+}
+
+function delete_id(id) {
+  if(confirm("Sure To Remove ?")){
+    window.location.href='index.php?delete_id='+id;
+  }
+}
+
+function closeitemForm() {
+  document.getElementById("StatusForm").style.display = "none";
+  document.getElementById("deleteform").style.display = "none";
+}
+
+/* Start Popup Form */
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function opentForm(name) {
+  var tname = name ;
+  document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+/* End Popup Form */
+
+/* New Script */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.filter__controls li').on('click', function () {
+            $('.filter__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.product__filter').length > 0) {
+            var containerEl = document.querySelector('.product__filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Search Switch
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Accordin Active
+    --------------------*/
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).prev().addClass('active');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).prev().removeClass('active');
+    });
+
+    //Canvas Menu
+    $(".canvas__open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("active");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("active");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    /*-----------------------
+        Hero Slider
+    ------------------------*/
+    $(".hero__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ["<span class='arrow_left'><span/>", "<span class='arrow_right'><span/>"],
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false
+    });
+
+    /*--------------------------
+        Select
+    ----------------------------*/
+    $("select").niceSelect();
+
+    /*-------------------
+		Radio Btn
+	--------------------- */
+    $(".product__color__select label, .shop__sidebar__size label, .product__details__option__size label").on('click', function () {
+        $(".product__color__select label, .shop__sidebar__size label, .product__details__option__size label").removeClass('active');
+        $(this).addClass('active');
+    });
+
+    /*-------------------
+		Scroll
+	--------------------- */
+    $(".nice-scroll").niceScroll({
+        cursorcolor: "#0d0d0d",
+        cursorwidth: "5px",
+        background: "#e5e5e5",
+        cursorborder: "",
+        autohidemode: true,
+        horizrailenabled: false
+    });
+
+    /*------------------
+        CountDown
+    --------------------*/
+    // For demo preview start
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(mm == 12) {
+        mm = '01';
+        yyyy = yyyy + 1;
+    } else {
+        mm = parseInt(mm) + 1;
+        mm = String(mm).padStart(2, '0');
+    }
+    var timerdate = mm + '/' + dd + '/' + yyyy;
+    // For demo preview end
+
+
+    // Uncomment below and use your date //
+
+    /* var timerdate = "2020/12/30" */
+
+    $("#countdown").countdown(timerdate, function (event) {
+        $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Seconds</p> </div>"));
+    });
+
+    /*------------------
+		Magnific
+	--------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*-------------------
+		Quantity change
+	--------------------- */
+    var proQty = $('.pro-qty');
+    proQty.prepend('<span class="fa fa-angle-up dec qtybtn"></span>');
+    proQty.append('<span class="fa fa-angle-down inc qtybtn"></span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
+    var proQty = $('.pro-qty-2');
+    proQty.prepend('<span class="fa fa-angle-left dec qtybtn"></span>');
+    proQty.append('<span class="fa fa-angle-right inc qtybtn"></span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
+    /*------------------
+        Achieve Counter
+    --------------------*/
+    $('.cn_num').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+})(jQuery);
+/* End New Script */
+
+(function($){
+
+    $(document).ready(function ($) {
+    
+         jQuery('#map').CustomMap();
+    
+    
+         ////------- Testimonials Carousel
+        
+        var testimonial = $(".testimonial-wrapper");
+        testimonial.owlCarousel({
+            pagination: false,
+            navigation : true,
+            slideSpeed : 1000,
+            stopOnHover: true,
+            autoPlay: 3000,
+            singleItem: true,
+            transitionStyle : "fade",
+            navigationText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+        });
+        
+        /*----------------------------------------------------*/
+        /*	Nav Menu & Search
+        /*----------------------------------------------------*/
+        
+        $(".nav > li:has(ul)").addClass("drop");
+        $(".nav > li.drop > ul").addClass("dropdown");
+        $(".nav > li.drop > ul.dropdown ul").addClass("sup-dropdown");
+        
+        
+        
+        
+        
+        /*---------------------------------------------------*/
+        /* Progress Bar
+        /*---------------------------------------------------*/
+        $(document).ready(function($) {
+        "use strict";
+    
+        
+        
+            $('.skill-shortcode').appear(function() {
+                $('.progress').each(function(){ 
+                    $('.progress-bar').css('width',  function(){ return ($(this).attr('data-percentage')+'%')});
+                });
+            },{accY: -100});
+            
+            
+        });    
+        
+        
+        /*--------------------------------------------------*/
+        /* Counter*/
+        /*--------------------------------------------------*/ 
+        $('.timer').countTo();
+    
+        $('.counter-item').appear(function() {
+            $('.timer').countTo();
+        },{accY: -100});
+        
+        
+    });
+    
+    }(jQuery));
